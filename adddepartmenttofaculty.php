983372660a2814f5056708e5bc21cd21753db6ab
Ghunter77 <144743913+Ghunter77@users.noreply.github.com>
2023-12-14 10:45:40 -0500

    Add files via upload

---
+++ adddepartmenttofaculty.php
@@ -0,0 +1,105 @@
+<?php
+@include 'config1.php';
+
+if (isset($_POST['facultyId'], $_POST['deptId'])) {
+    $facultyId = mysqli_real_escape_string($conn, $_POST['facultyId']);
+    $deptId = mysqli_real_escape_string($conn, $_POST['deptId']);
+
+    $roleQuery = "SELECT Position FROM faculty WHERE FacultyID = ?";
+    $roleStmt = mysqli_prepare($conn, $roleQuery);
+    mysqli_stmt_bind_param($roleStmt, 's', $facultyId);
+    mysqli_stmt_execute($roleStmt);
+    $roleResult = mysqli_stmt_get_result($roleStmt);
+    $roleRow = mysqli_fetch_assoc($roleResult);
+    $facultyRole = $roleRow['Position'];
+
+    if ($facultyRole === 'Manager' || $facultyRole === 'Chair') {
+        echo "Faculty members with the role of Manager or Chair cannot be added to another department.";
+    } else {
+        mysqli_begin_transaction($conn);
+
+        try {
+            $typeQuery = "SELECT FacultyType FROM faculty WHERE FacultyID = ?";
+            $typeStmt = mysqli_prepare($conn, $typeQuery);
+            mysqli_stmt_bind_param($typeStmt, 's', $facultyId);
+            mysqli_stmt_execute($typeStmt);
+            $typeResult = mysqli_stmt_get_result($typeStmt);
+            $typeRow = mysqli_fetch_assoc($typeResult);
+
+            if ($typeRow) {
+                $facultyType = $typeRow['FacultyType'];
+
+               
+            $teachingQuery = "SELECT COUNT(*) AS TeachingCount FROM coursesection cs 
+            JOIN course c ON cs.CourseID = c.CourseID 
+            WHERE cs.FacultyID = ? AND c.DeptID = ?";
+$teachingStmt = mysqli_prepare($conn, $teachingQuery);
+mysqli_stmt_bind_param($teachingStmt, 'ss', $facultyId, $deptId);
+mysqli_stmt_execute($teachingStmt);
+$teachingResult = mysqli_stmt_get_result($teachingStmt);
+$teachingRow = mysqli_fetch_assoc($teachingResult);
+
+if ($teachingRow && $teachingRow['TeachingCount'] > 0) {
+echo "Cannot remove faculty from a department they are currently teaching in.";
+                } else {
+                    $countQuery = "SELECT COUNT(*) AS DeptCount FROM facultydept WHERE FacultyID = ?";
+                    $countStmt = mysqli_prepare($conn, $countQuery);
+                    mysqli_stmt_bind_param($countStmt, 's', $facultyId);
+                    mysqli_stmt_execute($countStmt);
+                    $countResult = mysqli_stmt_get_result($countStmt);
+                    $countRow = mysqli_fetch_assoc($countResult);
+                    $deptCount = $countRow['DeptCount'];
+
+                  
+                if ($facultyType === 'Full-time' && $deptCount < 2) {
+                    if ($deptCount == 1) {
+                        $updateQuery = "UPDATE facultydept SET PercentTime = 50 WHERE FacultyID = ?";
+                        $updateStmt = mysqli_prepare($conn, $updateQuery);
+                        mysqli_stmt_bind_param($updateStmt, 's', $facultyId);
+                        mysqli_stmt_execute($updateStmt);
+                        mysqli_stmt_close($updateStmt);
+                    }
+
+                    $newPercentTime = ($deptCount == 0) ? 100 : 50;
+                    $insertQuery = "INSERT INTO facultydept (FacultyID, DeptID, PercentTime) VALUES (?, ?, ?)";
+                    $insertStmt = mysqli_prepare($conn, $insertQuery);
+                    mysqli_stmt_bind_param($insertStmt, 'ssi', $facultyId, $deptId, $newPercentTime);
+                    mysqli_stmt_execute($insertStmt);
+                    mysqli_stmt_close($insertStmt);
+
+                    echo "Department added to faculty successfully.";
+                } elseif ($facultyType === 'Part-time' && $deptCount == 0) {
+                    $insertQuery = "INSERT INTO facultydept (FacultyID, DeptID, PercentTime) VALUES (?, ?, 100)";
+                    $insertStmt = mysqli_prepare($conn, $insertQuery);
+                    mysqli_stmt_bind_param($insertStmt, 'ss', $facultyId, $deptId);
+                    mysqli_stmt_execute($insertStmt);
+                    mysqli_stmt_close($insertStmt);
+
+                    echo "Department added to faculty successfully.";
+                } else {
+                    echo "Cannot add department due to faculty type and existing department assignments.";
+                }
+                }
+            } else {
+                echo "Faculty member not found.";
+            }
+
+            mysqli_commit($conn);
+        } catch (Exception $e) {
+            mysqli_rollback($conn);
+            echo "Error: " . $e->getMessage();
+        }
+
+        mysqli_stmt_close($roleStmt);
+        mysqli_stmt_close($typeStmt);
+        mysqli_stmt_close($countStmt);
+        if (isset($teachingStmt)) {
+            mysqli_stmt_close($teachingStmt);
+        }
+    }
+} else {
+    echo "Faculty ID and Department ID are required.";
+}
+
+mysqli_close($conn);
+?>
